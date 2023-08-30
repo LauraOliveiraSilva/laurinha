79ae92eb49110ad11b73b21aa898507b2829846f
LauraOliveiraSilva <laura_oliveirajj12@hotmail.com>
2023-08-30 11:28:10 -0300

    att3008

---
+++ aula3008/3008ex5.js
@@ -0,0 +1,6 @@
+let texto = 'aprenda JavaScript, aprenda Python, aprenda JS'
+
+// Substituindo 'aprenda' por 'estude
+let novoTexto = texto.replace(/aprenda/g, 'estude');
+console.log(novoTexto); // Retorna 'Estude JavaScript, estude Python'
+
