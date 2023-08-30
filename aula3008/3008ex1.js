08568f78a567bb62eca9285b658ff4c5448ac7fb
LauraOliveiraSilva <laura_oliveirajj12@hotmail.com>
2023-08-30 11:30:09 -0300

    att3008

---
+++ aula3008/3008ex1.js
@@ -0,0 +1,16 @@
+let texto = 'Olá, mundo!';
+
+// Tamanho da string
+let tamanho = texto.length;
+console.log(tamanho); // Retorna 12
+
+// Acessando um caractere específico
+let primeiroCaractere = texto[0];
+console.log(primeiroCarac); // Retorna 0
+
+// Encontrando a posição de um caractere
+let texto1 = 'Olá, mundo!';
+
+// Encontrando a posição de um caractere
+let posicao = texto1.indexOf('mundo');
+console.log(posicao); // Retorna 5
---
+++ aula3008/3008ex3.js
@@ -0,0 +1,9 @@
+let texto = 'Texto para conversão';
+
+// Convertendo para maiúsculas
+let maiusculas = texto.toLocaleUpperCase();
+console.log(miusculas); // Retorna 'TEXTO PARA CONVERSÃP'
+
+// Convertendo para minúsculas
+let minusculas = texto.toLowerCase();
+console.log(minusculas); // Retorna 'texto para conversão'
